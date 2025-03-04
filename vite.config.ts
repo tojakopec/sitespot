babca1e42d58979bd73a9437691c6808aee60b43
Tomislav <tomislav.jakopec@gmail.com>
2025-03-04 12:37:28 -0500

    Change proxy settings. Add shared, public etc. paths to tsconfig.

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,29 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
       "@client": path.resolve(__dirname, "./src/client"),
       "@server": path.resolve(__dirname, "./src/server"),
+      "@shared": path.resolve(__dirname, "./src/shared"),
     },
   },
   build: {
     outDir: "dist",
     sourcemap: true,
+  },
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:3000",
+        changeOrigin: true,
+      },
+    },
   },
 });
